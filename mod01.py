589f08376b0b1fc4ca4dba58db032f11e64f9827
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-12-01 19:27:13 -0500

    A letter R will move from left to right across the screen.

---
+++ mod01.py
@@ -0,0 +1,18 @@
+r = "R"
+import os
+import time
+while True:
+    # Banner program to move the value of `r` across the screen
+    r = "R"
+    screen_width = 90  # Define the width of screen where the banner will move
+
+
+    def move_banner():
+        for position in range(screen_width):
+            os.system('clear' if os.name == 'posix' else 'cls')  # Clear screen
+            print(' ' * position + r)  # Move `r` to the right
+            time.sleep(0.1)  # Pause for a short period of time
+
+
+    # Run the banner
+    move_banner()
